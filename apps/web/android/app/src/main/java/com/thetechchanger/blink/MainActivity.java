982c579478affd9d2395f31fe21e329dffa85fa5
TiXX <sajhansakkir1@gmail.com>
2026-06-14 14:55:15 +0530

    Fix Node 20 and gradlew permissions

---
+++ apps/web/android/app/src/main/java/com/thetechchanger/blink/MainActivity.java
@@ -0,0 +1,5 @@
+package com.thetechchanger.blink;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.thetechchanger.blink',
+  appName: 'B-Link',
+  webDir: 'out'
+};
+
+export default config;
